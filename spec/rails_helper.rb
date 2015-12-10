aa93a69d88baedcc1cfe4fd2f6e61250618f46d7
hugo <a01191470@itesm.mx>
2015-12-10 15:01:27 -0600

    Refactors the json parse method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'support/request.rb'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -54,6 +55,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Including to test requests
+  config.include Request::JsonHelpers, :type => :controller
 end
 
 Shoulda::Matchers.configure do |config|
