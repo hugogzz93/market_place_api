e27877a296bb3256198e3019ae27ec910d1256e3
hugo <a01191470@itesm.mx>
2015-12-09 22:35:06 -0600

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  before {@user = FactoryGirl.build(:user)}
+  before { @user = FactoryGirl.build(:user) }
 
-  subject {@user}
-  it {should respond_to(:email)}
-  it {should respond_to(:password)}
-  it {should respond_to(:password_confirmation)}
+  subject { @user }
 
-  it {should be_valid}
+  # it { should respond_to(:email) }
+  # it { should respond_to(:password) }
+  # it { should respond_to(:password_confirmation) }
+
+  it { should validate_presence_of(:email) }
+	# it { should validate_uniqueness_of(:email) }
+	it { should validate_confirmation_of(:password) }
+	it { should allow_value('example@domain.com').for(:email) }
+
+  it { should be_valid }
 end
