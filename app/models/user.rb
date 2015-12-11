1a54363d455821ff688efbced0f4ca0056cc762a
hugo <a01191470@itesm.mx>
2015-12-10 22:01:35 -0600

    Finishes modeling the product model along with user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
     validates :auth_token, uniqueness: true
+    has_many :products, dependent: :destroy
 
     before_create :generate_authentication_token!
 
