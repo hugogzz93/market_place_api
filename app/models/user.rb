44ace0e64f71d2a37fbe640d9253280848491749
hugo <a01191470@itesm.mx>
2015-12-11 18:47:22 -0600

    Adds user order has many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
     validates :auth_token, uniqueness: true
     has_many :products, dependent: :destroy
+    has_many :orders, dependent: :destroy
 
     before_create :generate_authentication_token!
 
